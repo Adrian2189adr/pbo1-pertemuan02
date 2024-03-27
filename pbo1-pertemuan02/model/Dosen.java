b683b8ffef0c4edb00e298e7962165ee68bbcc79
Adrian2189adr <163805598+Adrian2189adr@users.noreply.github.com>
2024-03-27 15:33:49 +0700

    Add files via upload

---
+++ pbo1-pertemuan02/model/Dosen.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Dosen{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("== Data Dosen =================");
+  System.out.println("NIDN : 9812312189");
+  System.out.println("Nama : Gaidzan De Valeria");
+  System.out.println("===============================");
+ }
+}
---
+++ pbo1-pertemuan02/model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Mahasiswa{
+ public static void tampilanInfo(){
+  System.out.println();
+  System.out.println("-- Data Mahasiswa -----------------");
+  System.out.println("NIDN : 2210010542");
+  System.out.println("Nama : Muhammad Adrian Fadilah");
+  System.out.println("===================================");
+ }
+}
